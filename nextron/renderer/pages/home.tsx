867651f82ca504ad86e2c6e82b96bb404ec7ff4d
wlals <jiminelp@gmail.com>
2024-03-12 15:10:16 +0900

    ✨:: 내 이름 파싱

--- nextron/renderer/pages/home.tsx
+++ nextron/renderer/pages/home.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Head from 'next/head';
 import { Header } from '../components/header';
 import { Textplace } from '../components/textplace';
 import { Main } from '../components/chatBox';
 import { messageMapper } from '../utils/function/messagesMapper';
-import { getMessages } from '../utils/Mock/getMessage';
 import { connectSocket } from '../utils/function/socketConnect';
 import { MessageData } from '../utils/types/types';
 import { Socket } from 'socket.io-client';
@@ -13,6 +12,9 @@ export default function HomePage() {
   const [opportunity, setOpportunity] = useState<number>(30);
   const [chats, setChats] = useState<React.ReactNode[]>([]);
   const [socket, setSocket] = useState<Socket | null>(null);
+  const opponentNameRef = useRef<string>('');
+  const [myName, setMyName] = useState<string>('');
+  const lastMessageRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const newSocket = connectSocket();
@@ -20,6 +22,16 @@ export default function HomePage() {
     newSocket.emit('joinRoom', 'room1');
     newSocket.on('roomUsers', (users: string[]) => {
       console.log(users);
+      if (users.length == 2) {
+        opponentNameRef.current = users[0];
+        setMyName(users[1]);
+      } else {
+        setMyName(users[0]);
+      }
+    });
+    newSocket.on('joinAnother', userName => {
+      console.log(`${userName}이 들어왔습니다`);
+      opponentNameRef.current = userName;
     });
 
     return () => {
@@ -30,10 +42,14 @@ export default function HomePage() {
   useEffect(() => {
     const handleMessage = (data: MessageData) => {
       const newChats = messageMapper(
-        [{ message: data.message, sender: '무명' }],
+        [{ message: data.message, sender: opponentNameRef.current }],
         opportunity,
+        lastMessageRef,
+        myName,
       ).mappedContent;
+      lastMessageRef.current;
       setChats(prevChats => [...prevChats, ...newChats]);
+      setOpportunity(opportunity - 1);
     };
 
     if (socket) {
@@ -42,13 +58,7 @@ export default function HomePage() {
         socket.off('message', handleMessage);
       };
     }
-  }, [socket, opportunity]);
-
-  useEffect(() => {
-    const { mappedContent, remainingOpportunity } = messageMapper(getMessages(), opportunity);
-    setChats(mappedContent);
-    setOpportunity(remainingOpportunity);
-  }, []);
+  }, [socket, opportunity, opponentNameRef]);
 
   return (
     <React.Fragment>
@@ -57,7 +67,13 @@ export default function HomePage() {
       </Head>
       <Header opportunity={opportunity} />
       <Main>{chats}</Main>
-      <Textplace setChats={setChats} opportunity={opportunity} setOpportunity={setOpportunity} />
+      <Textplace
+        setChats={setChats}
+        opportunity={opportunity}
+        setOpportunity={setOpportunity}
+        lastMessageRef={lastMessageRef}
+        myName={myName}
+      />
     </React.Fragment>
   );
 }
