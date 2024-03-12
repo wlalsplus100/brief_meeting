c09dbd5c479c0d1f8cf63df653e01967ccaeb0f8
wlals <jiminelp@gmail.com>
2024-03-12 15:03:40 +0900

    🔥:: console.log 삭제

--- nextron/renderer/utils/function/sendMessage.ts
+++ nextron/renderer/utils/function/sendMessage.ts
@@ -2,6 +2,5 @@ import { connectSocket } from './socketConnect';
 
 export const sendMessage = async (msg: string) => {
   const socket = connectSocket();
-  console.log('메세지 보냄');
   socket.emit('sendMessage', { message: msg, room: 'room1' });
 };
